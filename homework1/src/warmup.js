46e79c040d16e46cd838968e8b0df72c315801f5
Juan Neri <Juanjo.neri@gmail.com>
2017-09-03 15:55:21 -0700

    make permutations unig swapping

--- homework1/src/warmup.js
+++ homework1/src/warmup.js
@@ -18,3 +18,20 @@ exports.change = function(cents) {
 exports.stripQuotes = function(str) {
     return str.replace(/["'\\]/g, '');
 }
+
+exports.scramble = function(str) {
+    const MAX_N_SWAPS = 100;
+    const N_SWAPS = Math.floor(Math.random() * MAX_N_SWAPS);
+    const STR_LEN = str.length;
+
+    for (let i = 0; i < N_SWAPS; ++i) {
+        let pos1 = Math.floor(Math.random() * STR_LEN);
+        let pos2 = Math.floor(Math.random() * STR_LEN);
+        let char1 = str[pos1];
+        let char2 = str[pos2];
+        str = str.replace(str[pos1], char2);
+        str = str.replace(str[pos2], char1);
+    }
+
+    return str;
+}
