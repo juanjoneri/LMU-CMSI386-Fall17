9347285062f94ad4e19d03b0cfa9bfbe7eb7f775
Juan Neri <Juanjo.neri@gmail.com>
2017-09-03 12:20:13 -0700

    compute efficient solution unig division

--- homework1/src/warmup.js
+++ homework1/src/warmup.js
@@ -1,5 +1,17 @@
 
 
-exports.change = function() { 
-    return [0,0,0,0];
+exports.change = function(cents) {
+    if (cents < 0) { throw new RangeError(); }
+
+    const coins = [25, 10, 5, 1];
+    var ans = [0, 0, 0, 0];
+
+
+    var i = 0;
+    coins.forEach(function(coin) {
+        ans[i] = Math.floor(cents / coins[i]);
+        cents -= ans[i]*coins[i];
+        i++;
+    });
+    return ans;
 };
