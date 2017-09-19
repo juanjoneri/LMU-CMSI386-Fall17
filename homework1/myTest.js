830beccd8716db929deb5bc64fa2cd00264a4109
Natalia Dibbern <ndibbern@natalias-mbp-2.attlocal.net>
2017-09-18 20:51:41 -0700

    make a version for macos just in case

--- homework1/myTest.js
+++
@@ -1,7 +0,0 @@
-const {
-  change, stripQuotes, scramble, say, powers, interleave,
-  powersGenerator, cylinder, makeCryptoFunctions, randomName,
-} = require('./src/warmup.js');
-
-promise = randomName({ gender: 'femalea', region: 'france' });
-promise.then(console.log).catch((err) => {console.log(err.message + " this is tit")});
