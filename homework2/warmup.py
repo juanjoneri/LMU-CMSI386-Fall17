6a673c26fb98672f64b0ca62991cb88ee88936c6
Natalia <ndibbern@lion.lmu.edu>
2017-09-26 17:10:00 -0700

    add some changes sugested in class

--- homework2/warmup.py
+++ homework2/warmup.py
@@ -4,7 +4,6 @@ import re
 def change(cents):
     if cents < 0:
         raise ValueError('amount cannot be negative')
-
     ans = [0] * 4
     i = 0;
     for coin in [25, 10, 5, 1]:
@@ -29,33 +28,30 @@ def triples(value):
     return 0
 
 def powers(base, value):
+    #use yield
     return 0
 
-def interleave():
+def interleave(a, *b):
     return 0
 
     import math
 
 class Cylinder:
     "A circle with a 2-D center point and a radius."
-
     def __init__(self, radius = 1, height = 1):
         self.height = height
         self.radius = radius
-
+    @property
     def volume(self):
         "Returns the volume of the Cylinder"
-        return math.pi * (self.radius ** 2) * height
-
+        return math.pi * (self.radius ** 2) * self.height
     def area(self):
         "Returns the surface area of the Cylinder"
-        return (math.pi * self.radius * 2 * height) + (2 * math.pi * (self.radius ** 2))
-
+        return (math.pi * self.radius * 2 * self.height) + (2 * math.pi * (self.radius ** 2))
     def widen(self, factor):
         "Increases the radius by the given factor"
         self.radius *= factor
         return self
-
     def stretch(self, factor):
         "Increases the radius by the given factor"
         self.height *= factor
@@ -64,5 +60,6 @@ class Cylinder:
 def make_crypto_functions():
     return 0
 
-def random_name():
+def random_name(region=None, gender=None):
+
     return 0
