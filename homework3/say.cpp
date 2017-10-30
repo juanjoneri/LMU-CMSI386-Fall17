47f990189b1b3a6cd0c3fa2a5ac87b87c9ba96db
Juan Neri <Juanjo.neri@gmail.com>
2017-10-29 21:24:11 -0700

    new approach that prints all values sequentially

--- homework3/say.cpp
+++ homework3/say.cpp
@@ -9,22 +9,20 @@ class Say {
 
     public:
         Say(string first_word): phrase(first_word = "") {}
-        auto operator()(string new_word = "") {
-            // if (new_word == "") {
-            //     return phrase;
-            // }
-
-            return [&](string new_word = "") {
-                Say::phrase += ' ';
-                Say::phrase += new_word;
-                return Say::phrase;
-            };
+        auto operator()(string new_word) {
+            phrase += new_word;
+            cout << phrase << endl;
+            Say say(phrase);
+            return say;
+        }
+        auto operator()() {
+            return phrase;
         }
 };
 
 int main() {
   Say say("first");
-  cout << say("hi")("to") << endl;
+  cout << say("hi")("there")() << endl;
 
   // assert(say('hi')() == 'hi');
   // assert(say('hi')('there')() == 'hi there');
