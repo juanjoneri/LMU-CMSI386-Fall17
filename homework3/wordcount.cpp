e40267fc443602b7f0bd9bec36cafde7dab58256
Juan Neri <Juanjo.neri@gmail.com>
2017-10-29 19:27:23 -0700

    clean code change  names and remove unecesary

--- homework3/wordcount.cpp
+++ homework3/wordcount.cpp
@@ -5,31 +5,35 @@
 #include <unordered_map>
 using namespace std;
 
-void insert(unordered_map<string, int> &map1, string word) {
-    if(map1.find(word) != map1.end())
-        map1[word]++;
-    else
-        map1.insert(unordered_map<string,int>::value_type(word,1));
+// Inserts a new word in a map, to keep track of the occurences of that word
+void insert(unordered_map<string, int> &destination_map, string word) {
+    if (destination_map.find(word) != destination_map.end()) {
+        destination_map[word]++;
+    } else {
+        destination_map.insert(unordered_map<string,int>::value_type(word, 1));
+    }
 }
 
 int main(int argc, char** argv) {
     char letter;
     string word;
-    unordered_map<string, int> theOriginalMap;
+    unordered_map<string, int> word_record;
 
 
     while (cin.get(letter)) {
         if (isspace(letter) || letter == ',' || letter == '.') {
             if (word.length() != 0) {
-                insert(theOriginalMap, word);
+                insert(word_record, word);
             }
             word = "";
         } else if (isalpha(letter)) {
             word += tolower(letter);
+        } else {
+            // In the case that the character is a nuber or other symbol, we just ignore it
         }
     }
 
-    for (auto& x: theOriginalMap) {
-        std::cout << x.first << ": " << x.second << std::endl;
+    for (auto& record: word_record) {
+        cout << record.first << " " << record.second << endl;
     }
 }
