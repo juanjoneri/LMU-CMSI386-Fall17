db54ea3a820f148c287bdd29d032c136344191f6
Juan Neri <Juanjo.neri@gmail.com>
2017-12-02 00:28:02 -0500

    dry datePat

--- homework4/testing/daysBetween.elm
+++ homework4/testing/daysBetween.elm
@@ -3,10 +3,14 @@ import List exposing (reverse, map, sum, filter)
 import Regex exposing (..)
 import Date exposing (..)
 
+datePat = regex "(\\d{4})-(\\d{2})-(\\d{2})"
 
-daysBetween: String -> Bool
-daysBetween  d1 =
-    contains (regex "\\d{4}-\\d{2}-\\d{2}") d1
+daysBetween  d1 d2 =
+    if contains datePat d1 && contains datePat d2
+    then
+        1
+    else
+        0
 
 main =
-      daysBetween "2016-02-27" |> toString |> text
+      daysBetween "2016-02-27" "2016-03-01" |> toString |> text
