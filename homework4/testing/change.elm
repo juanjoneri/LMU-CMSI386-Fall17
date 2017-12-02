474db45074a5a393d981f2779a46c11c0ee6b485
Juan Neri <Juanjo.neri@gmail.com>
2017-12-02 09:27:10 -0500

    change working. Not very dry

--- homework4/testing/change.elm
+++ homework4/testing/change.elm
@@ -11,7 +11,13 @@ change amount =
         let
             makeChange q n d p remainder =
                 if remainder == 0 then
-                    Ok (p)
+                    Ok (q, n, d, p)
+                else if remainder >= 25 then
+                    makeChange (remainder//25) n d p (remainder%25)
+                else if remainder >= 10 then
+                    makeChange q (remainder//10) d p (remainder%10)
+                else if remainder >= 5 then
+                    makeChange q n (remainder//5) p (remainder%5)
                 else
                     makeChange q n d (p+1) (remainder-1)
         in
