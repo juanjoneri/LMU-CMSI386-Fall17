1bc5fe36ca5c701e1bfd1edb817b5a52f6364bff
Natalia <ndibbern@lion.lmu.edu>
2017-12-01 22:59:57 -0500

    powers work but need to tweak for tests to pass

--- homework4/Warmup.elm
+++ homework4/Warmup.elm
@@ -2,6 +2,7 @@ module Warmup exposing (..)
 import String exposing (toList, fromList)
 import List exposing (concatMap, map, foldr)
 import List exposing (reverse, map, repeat)
+import Result exposing ( Result( Ok, Err ) )
 
 change : Int -> Int
 change a =
@@ -14,11 +15,12 @@ stripQuotes a =
 powers : Int -> Int -> List Int
 powers base limit =
     let accumulate pows =
+        if limit < 1 then [] else
         case pows of
             x :: rest -> if base * x > limit then pows else accumulate (base * x::pows)
             _ -> []
     in
-        reverse (accumulate [base, 1])
+    reverse (accumulate [base, 1])
 
 sumOfCubesOfOdds : Int -> Int
 sumOfCubesOfOdds a =
