8a6fe49cb6b9f8ff4e57daba3da0e0e18eba06e6
Juan Neri <Juanjo.neri@gmail.com>
2017-12-02 00:50:47 -0500

    better err feedback

--- homework4/testing/daysBetween.elm
+++ homework4/testing/daysBetween.elm
@@ -3,18 +3,16 @@ import List exposing (reverse, map, sum, filter)
 import Regex exposing (..)
 import Date exposing (..)
 
-datePat = regex "(\\d{4})-(\\d{2})-(\\d{2})"
+millisecondsPerDay =
+    24 * 60 * 60 * 1000
+
 
 daysBetween  d1 d2 =
-    if contains datePat d1 && contains datePat d2
-    then
-        case (d1 |> fromString, d2 |> fromString) of
-            (Ok value1, Ok value2) -> Err "1"
-            (Err msg1, Ok value2) -> Err "2"
-            (Ok value1, Err msg2) -> Err "3"
-            (Err msg1, Err msg2) -> Err msg1
-    else
-        Err "msg"
+    case (d1 |> fromString, d2 |> fromString) of
+        (Ok value1, Ok value2) -> Ok (((value1 |> toTime) - (value2 |> toTime)) / millisecondsPerDay)
+        (Err msg1, Ok value2) -> Err (d1 ++ " is not a date.")
+        (Ok value1, Err msg2) -> Err (d2 ++ " is not a date.")
+        (Err msg1, Err msg2) -> Err ("Neither " ++ d1 ++ " nor " ++ d2 ++ " are dates.")
 
 main =
       daysBetween "2016-02-27" "2016-03-01" |> toString |> text
