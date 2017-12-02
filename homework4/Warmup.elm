55eb23826d4ae561264009e10cd89c659baf006e
Natalia <ndibbern@lion.lmu.edu>
2017-12-01 22:41:06 -0500

    first attemt to do powers not working for neg

--- homework4/Warmup.elm
+++ homework4/Warmup.elm
@@ -1,6 +1,7 @@
 module Warmup exposing (..)
 import String exposing (toList, fromList)
 import List exposing (concatMap, map, foldr)
+import List exposing (reverse, map, repeat)
 
 change : Int -> Int
 change a =
@@ -10,9 +11,14 @@ stripQuotes : Int -> Int
 stripQuotes a =
     1
 
-powers : Int -> Int
-powers a =
-    1
+powers : Int -> Int -> List Int
+powers base limit =
+    let accumulate pows =
+        case pows of
+            x :: rest -> if base * x > limit then pows else accumulate (base * x::pows)
+            _ -> []
+    in
+        reverse (accumulate [base, 1])
 
 sumOfCubesOfOdds : Int -> Int
 sumOfCubesOfOdds a =
