2378013ed563fd55afe5317b2ce60dbf1ccc769e
Juan Neri <Juanjo.neri@gmail.com>
2017-12-02 00:43:03 -0500

    o yes, pattern match errors from fromString

--- homework4/testing/daysBetween.elm
+++ homework4/testing/daysBetween.elm
@@ -8,9 +8,13 @@ datePat = regex "(\\d{4})-(\\d{2})-(\\d{2})"
 daysBetween  d1 d2 =
     if contains datePat d1 && contains datePat d2
     then
-        1
+        case (d1 |> fromString, d2 |> fromString) of
+            (Ok value1, Ok value2) -> Err "1"
+            (Err msg1, Ok value2) -> Err "2"
+            (Ok value1, Err msg2) -> Err "3"
+            (Err msg1, Err msg2) -> Err msg1
     else
-        0
+        Err "msg"
 
 main =
       daysBetween "2016-02-27" "2016-03-01" |> toString |> text
