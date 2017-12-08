8296dfb8694632ca871992bffc37938c0ae8c3c3
Natalia <ndibbern@lion.lmu.edu>
2017-12-08 11:11:07 -0800

    clean some imports

--- homework4/Warmup.elm
+++ homework4/Warmup.elm
@@ -4,8 +4,8 @@ import List exposing (concatMap, map, foldr, sum, filter)
 import List exposing (reverse, map, repeat)
 import Result exposing ( Result( Ok, Err ) )
 import Html exposing (Html, ul, li, text)
-import Regex exposing (..)
-import Date exposing (..)
+import Regex exposing (regex, replace)
+import Date exposing (fromString, toTime)
 
 coins: (Int, Int, Int, Int)
 coins =
@@ -29,12 +29,11 @@ change amount =
 
 stripQuotes : String -> String
 stripQuotes =
-    replace All (regex "['\"]") (\_ -> "")
+    replace Regex.All (regex "['\"]") (\_ -> "")
 
 powers : Int -> Int -> Result String (List Int)
 powers base limit =
     if base < 0 then Err "negative base" else
-
     let accumulate pows =
         if limit < 1 then [] else
         case pows of
@@ -52,10 +51,10 @@ sumOfCubesOfOdds numbers =
         |> sum
 
 daysBetween : String -> String -> Result String Float
-daysBetween  d1 d2 =
+daysBetween  date1 date2 =
     let millisecondsPerDay = 24 * 60 * 60 * 1000 in
-        case (d1 |> fromString, d2 |> fromString) of
+        case (date1 |> fromString, date2 |> fromString) of
             (Ok value1, Ok value2) -> Ok (((value2 |> toTime) - (value1 |> toTime)) / millisecondsPerDay)
-            (Err _, Ok _) -> Err (d1 ++ " is not a date.")
-            (Ok _, Err _) -> Err (d2 ++ " is not a date.")
-            (Err _, Err _) -> Err ("Neither " ++ d1 ++ " nor " ++ d2 ++ " are dates.")
+            (Err _, Ok _) -> Err (date1 ++ " is not a date.")
+            (Ok _, Err _) -> Err (date2 ++ " is not a date.")
+            (Err _, Err _) -> Err ("Neither " ++ date1 ++ " nor " ++ date2 ++ " are dates.")
