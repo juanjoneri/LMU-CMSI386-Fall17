4e3867e2bf954bf26a938d7c3947c5c33d24acd2
Natalia <ndibbern@lion.lmu.edu>
2017-12-08 09:59:38 -0800

    remove fn milliseconds and put it in let

--- homework4/Warmup.elm
+++ homework4/Warmup.elm
@@ -55,14 +55,11 @@ sumOfCubesOfOdds lista =
         |> map (\x -> x ^ 3)
         |> sum
 
-millisecondsPerDay : Float
-millisecondsPerDay =
-    24 * 60 * 60 * 1000
-
 daysBetween : String -> String -> Result String Float
 daysBetween  d1 d2 =
-    case (d1 |> fromString, d2 |> fromString) of
-        (Ok value1, Ok value2) -> Ok (((value2 |> toTime) - (value1 |> toTime)) / millisecondsPerDay)
-        (Err _, Ok _) -> Err (d1 ++ " is not a date.")
-        (Ok _, Err _) -> Err (d2 ++ " is not a date.")
-        (Err _, Err _) -> Err ("Neither " ++ d1 ++ " nor " ++ d2 ++ " are dates.")
+    let millisecondsPerDay = 24 * 60 * 60 * 1000 in
+        case (d1 |> fromString, d2 |> fromString) of
+            (Ok value1, Ok value2) -> Ok (((value2 |> toTime) - (value1 |> toTime)) / millisecondsPerDay)
+            (Err _, Ok _) -> Err (d1 ++ " is not a date.")
+            (Ok _, Err _) -> Err (d2 ++ " is not a date.")
+            (Err _, Err _) -> Err ("Neither " ++ d1 ++ " nor " ++ d2 ++ " are dates.")
